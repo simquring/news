2abef28da26c22deed6fc4b5d0216376333adc4e
Videyhubs <stakemail91@gmail.com>
2026-04-19 00:02:07 +0700

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,11 @@
 const API_URL = "https://news.stakemail91.workers.dev/";
 
 async function getNews() {
-  const res = await fetch(API_URL);
-  return await res.json();
+  const apiNews = await fetch(API_URL).then(r => r.json());
+
+  const manual = JSON.parse(localStorage.getItem("manual") || "[]");
+
+  return {
+    articles: [...manual, ...apiNews.articles]
+  };
 }
