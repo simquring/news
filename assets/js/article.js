0147bae8ae7b22997c9b054f5a733d9b5594ce11
Videyhubs <stakemail91@gmail.com>
2026-04-19 00:24:53 +0700

    Update article.js

--- assets/js/article.js
+++ assets/js/article.js
@@ -1,13 +1,10 @@
 const params = new URLSearchParams(location.search);
 const url = params.get("url");
 
-async function loadArticle() {
-  const res = await fetch("https://news.stakemail91.workers.dev/?detail=" + encodeURIComponent(url));
-  const data = await res.json();
-
-  document.getElementById("title").innerText = data.title;
-  document.getElementById("image").src = data.urlToImage;
-  document.getElementById("content").innerText = data.content;
-}
-
-loadArticle();
+fetch("https://news.stakemail91.workers.dev/detail?url=" + encodeURIComponent(url))
+.then(r => r.json())
+.then(d => {
+  document.getElementById("title").innerText = d.title;
+  document.getElementById("content").innerText = d.content;
+  document.getElementById("source").href = d.source;
+});
